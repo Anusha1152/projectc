d8f33c53d469b3e5255762c55d7806cb3b6b2ddf
gg <gg@DESKTOP-NS871KM>
2023-10-26 10:52:59 +0530

    first commit

---
+++ CollegeProject1/src/main/java/com/cg/co/CollegeProject1Application.java
@@ -0,0 +1,13 @@
+package com.cg.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeProject1Application.class, args);
+	}
+
+}
---
+++ CollegeProject1/src/test/java/com/cg/co/CollegeProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
